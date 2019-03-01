c40ff65f2afad6cde2f1cb8587408241a84bb425
ODA Hirohito <oda@i3-systems.com>
2019-03-02 07:00:08 +0900

    version 0.2.1

--- lib/hazama_kuroo.rb
+++ lib/hazama_kuroo.rb
@@ -4,34 +4,31 @@ require "hazama_kuroo/version"
 module HazamaKuroo
   # 実行メソッド
   def self.run
-    cards = generate_cards
+    cs = gen
     puts 'Game Start'
-    dealer = default_deck cards
-    puts "HZM: #{dealer[0][0]} ***"
-    you = default_deck cards
-    puts "YOU: #{you[0][0]} #{you[1][0]}"
-    one_or_eleven you[0]
-    stop_if_ope you[0]
-    one_or_eleven you[1]
-    stop_if_ope you[1]
+    hk = dft cs
+    puts "HZM: #{hk[0][0]} ***"
+    yu = dft cs
+    puts "YOU: #{yu[0][0]} #{yu[1][0]}"
+    ooe yu[0]
+    sio yu[0]
+    ooe yu[1]
+    sio yu[1]
 
-    flg = 1
-    while flg
-      puts 'Hit? [y/n]'
-      while str = gets
-        case str.chomp
+    f = 1
+    while f do
+      print 'Hit? [y/n] '
+      while s = gets do
+        case s.chomp
         when 'y'
-          you << (deal cards)
-          puts "YOU: #{you[-1][0]}"
-          one_or_eleven you[-1]
-          stop_if_ope you[-1]
-          if 21 < (sum you)
-            puts lose
-            flg = false
-          end
+          yu << (deal cs)
+          puts "YOU: #{yu[-1][0]}"
+          ooe yu[-1]
+          sio yu[-1]
+          (puts 'Lose'; f = false) if 21 < (sum yu)
         when 'n'
-          stand you, dealer, cards
-          flg = false
+          std yu, hk, cs
+          f = false
         else
           puts ope
           exit
@@ -42,7 +39,7 @@ module HazamaKuroo
   end
 
   # カードを 52 枚用意する
-  def self.generate_cards
+  def self.gen
     ms = %w[S C H D]
     c = []
     ms.each do |m|
@@ -67,29 +64,27 @@ module HazamaKuroo
   end
 
   # 初期デッキを作る
-  def self.default_deck cs
+  def self.dft cs
     [] << (deal cs) << (deal cs)
   end
 
   # カードを配る
   def self.deal cs
-    while 1
+    while 1 do
       m = rand(4)
       n = rand(11)
-      if 1 != cs[m][n][2]
-        cs[m][n][2] = 1
-        break
-      end
+      (cs[m][n][2] = 1; break) if 0 == cs[m][n][2]
     end
     cs[m][n]
   end
 
   # Ace をどうするか
-  def self.one_or_eleven a
+  def self.ooe a
     if 'A' == a[1] && 0 == a[3]
-      puts "Is #{a[0]} 1 or 11?"
-      str = gets
-      case str.chomp
+
+      print "#{a[0]}? [1/11] "
+      s = gets
+      case s.chomp
       when '1'
         a[1] = 1
       when '11'
@@ -102,7 +97,7 @@ module HazamaKuroo
   end
 
   # 不正操作だったら強制終了
-  def self.stop_if_ope c
+  def self.sio c
     if c[1].nil?
       puts ope
       exit
@@ -110,10 +105,10 @@ module HazamaKuroo
   end
 
   # 勝負する
-  def self.stand y, d, cs
+  def self.std y, d, cs
     a = sum y
     puts "Hole Card: #{d[1][0]}"
-    b = deal_by_dealer d, cs
+    b = dbd d, cs, a
     if 21 < b || b < a
       puts 'Win!'
     elsif a < b
@@ -133,26 +128,14 @@ module HazamaKuroo
   end
 
   # stand 後のディーラーの作業
-  def self.deal_by_dealer d, cs
+  def self.dbd d, cs, a
     d[0][1] = 11 if 'A' == d[0][1]
-    if 'A' == d[1][1]
-      if 11 == d[0][1]
-        s = d[0][1] + 1
-      else
-        s = d[0][1] + 11
-      end
-    else
-      s = d[0][1] + d[1][1]
-    end
+    s = d[0][1] + ('A' == d[1][1] ? (11 == d[0][1] ? 1 : 11) : d[1][1])
 
-    while s < 17
+    while s < 17 || s <= a do
       t = deal cs
       puts "HZM: #{t[0]}"
-      if 'A' == t[1]
-        s += 1
-      else
-        s += t[1]
-      end
+      s += ('A' == t[1] ? (s < 10 ? 11 : 1) : t[1])
     end
     s
   end
--- lib/hazama_kuroo/version.rb
+++ lib/hazama_kuroo/version.rb
@@ -1,3 +1,3 @@
 module HazamaKuroo
-  VERSION = "0.2.0"
+  VERSION = "0.2.1"
 end
